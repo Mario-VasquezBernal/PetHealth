fb7e14e85833db15d771dffad05eb0bce8a6ff70
Mario-VasquezBernal <mxvasquez@sudamericano.edu.ec>
2026-02-01 04:50:13 -0500

    asssd

--- server/jobs/cronJobs.js
+++ server/jobs/cronJobs.js
@@ -1,27 +1,8 @@
-const cron = require("node-cron");
-const {
-  sendAppointmentReminders,
-  sendVaccineReminders,
-  sendTaskReminders
-} = require("../utils/notificationService");
+const startAppointmentReminderCron = require("../cron/appointmentReminder");
 
 const initCronJobs = () => {
-  cron.schedule("0 9 * * *", async () => {
-    console.log("⏰ Ejecutando job: Recordatorios de citas");
-    await sendAppointmentReminders();
-  });
-
-  cron.schedule("0 10 * * 1", async () => {
-    console.log("⏰ Ejecutando job: Recordatorios de vacunas");
-    await sendVaccineReminders();
-  });
-
-  cron.schedule("0 8 * * *", async () => {
-    console.log("⏰ Ejecutando job: Recordatorios de tareas");
-    await sendTaskReminders();
-  });
-
-  console.log("✅ Cron jobs inicializados correctamente");
+  startAppointmentReminderCron();
+  console.log("✅ Cron jobs inicializados correctamente (solo citas)");
 };
 
 module.exports = initCronJobs;
